a75190a930c12bd025ed14b81ff1f173174b32eb
Georgiy Tarasov <georgiy@ridgebox.com>
2022-01-08 13:32:40 +0300

    Replace deprecated option

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   jsxSingleQuote: false,
   trailingComma: 'es5',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
+  bracketSameLine: false,
   arrowParens: 'always',
   singleQuote: true,
 };
